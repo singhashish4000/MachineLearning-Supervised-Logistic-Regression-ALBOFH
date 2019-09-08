91a05fc98b6782bc902d20810a1e03f4c29dc600
Ashish Singh <singhashish4000@gmail.com>
2019-09-08 13:31:44 +0530

    completed modeling of model

--- approve_loan_project_model.py
+++ approve_loan_project_model.py
@@ -15,6 +15,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 import random
+import time
 
 random.seed(100)
 
@@ -165,6 +166,120 @@ accuracies = cross_val_score(estimator=classifier,
 
 accuracies.mean()
 print("Random Forest Classifier Accuracy: %0.2f (+/- %0.2f)" % (accuracies.mean(), accuracies.std()*2))
+
+### Parameter Tuning
+
+# Applying Grid Search
+
+# Round 1: Entropy
+parameteres = {"max_depth": [3, None],
+               "max_features": [1, 5, 10],
+               "min_samples_split": [2, 5 ,10],
+               "min_samples_leaf": [1, 5, 10],
+               "bootstrap": [True, False],
+               "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameteres,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+
+t0 = time.thread_time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.thread_time()
+print("Took %0.2f seconds" % (t1-t0))
+
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+# Round 2: Entropy
+parameteres = {"max_depth": [None],
+               "max_features": [3, 5, 7],
+               "min_samples_split": [8, 10, 12],
+               "min_samples_leaf": [1, 2, 3],
+               "bootstrap": [True],
+               "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameteres,
+                           scoring = "accuracy",
+                           cv = 5,
+                           n_jobs = 1)
+
+t0 = time.thread_time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.thread_time()
+print("Took %0.2f seconds" % (t1-t0))
+
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+
+
+# PRedicting Test Set
+
+y_pred = grid_search.predict(X_test)
+
+# Evaluating Results
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+random_forest_results = pd.DataFrame([['RandomForest (n=100, GS*2 + Entropy)', acc, prec, rec, f1]], columns = ['Model', 'Accuracy', 'Precesion', 'Recall', 'F1 Score'])
+
+results = results.append(random_forest_results, ignore_index = True)
+
+results
+
+### End of Results ###
+
+# Formatting Final Results
+
+final_results = pd.concat([y_test, users], axis=1).dropna()
+final_results['predictions'] = y_pred
+final_results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
